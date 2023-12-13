cda297998e8d9c79b30188ea6031a697a4af15fd
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-13 19:36:50 +0300

    feat : task1.1 complite

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
 public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+    }
 
+    public static void printThreeWords() {
+        System.out.printf("Orange%nBanana%nApple");
+    }
 }
