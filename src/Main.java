1ee3d308547525bc090de75dccafadf3cf587a97
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-13 19:45:18 +0300

    feat : task1.2 complite

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
-        System.out.printf("Orange%nBanana%nApple");
+        System.out.printf("Orange%nBanana%nApple%n");
+    }
+
+    public static void checkSumSign() {
+        int a = 3;
+        int b = 4;
+        if ( a + b >= 0) System.out.println("Сумма положительная");
+            else System.out.println("Сумма отрицательная");
     }
 }
