95798b9fcbfc9176ef51e688d8b8ddd1175b5015
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-13 19:52:41 +0300

    feat : task1.3 complite

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
+        printColor();
     }
 
     public static void printThreeWords() {
@@ -13,5 +14,12 @@ public class Main {
         int b = 4;
         if ( a + b >= 0) System.out.println("Сумма положительная");
             else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 1000;
+        if ( value <= 0) System.out.println("Красный");
+        if ( (value > 0) && (value <= 100)) System.out.println("Желтый");
+        if ( value > 100) System.out.println("Зеленый");
     }
 }
