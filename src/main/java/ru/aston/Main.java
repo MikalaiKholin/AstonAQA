329474a51676046580b81efe8defbd2d8775217f
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-14 23:53:46 +0300

    move to Maven

---
+++ src/main/java/ru/aston/Main.java
@@ -0,0 +1,35 @@
+package ru.aston;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.printf("Orange%nBanana%nApple%n");
+    }
+
+    public static void checkSumSign() {
+        int a = 3;
+        int b = 4;
+        if (a + b >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor() {
+        int value = 1000;
+        if (value <= 0) System.out.println("Красный");
+        if ((value > 0) && (value <= 100)) System.out.println("Желтый");
+        if (value > 100) System.out.println("Зеленый");
+    }
+
+    public static void compareNumbers() {
+        int a = 5;
+        int b = 4;
+        if (a >= b) System.out.println("a >= b");
+        else System.out.println("a < b");
+    }
+}
