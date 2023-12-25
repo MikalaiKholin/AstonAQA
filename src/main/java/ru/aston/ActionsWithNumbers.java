388d1d0dbd0880aab9b2e6499852d8f19e53c0fc
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-25 19:46:15 +0300

    Lesson3_2

--- src/main/java/ru/aston/ActionsWithNumbers.java
+++ src/main/java/ru/aston/ActionsWithNumbers.java
@@ -4,4 +4,8 @@ public class ActionsWithNumbers {
     public static boolean isSummInRange(int a, int b){
         return (((a + b) >= 10) && ((a + b) <= 20));
     }
+
+    public static void isPositiveNumber(int number){
+        System.out.println(number >= 0 ? "positive" : "negative");
+    }
 }
--- src/main/java/ru/aston/Main.java
+++ src/main/java/ru/aston/Main.java
@@ -3,5 +3,6 @@ package ru.aston;
 public class Main {
     public static void main(String[] args) {
         System.out.println(ActionsWithNumbers.isSummInRange(1, 8));
+        ActionsWithNumbers.isPositiveNumber(0);
     }
 }
