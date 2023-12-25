d4770f2d1ccca8808e1d155ad8721185e672f661
MikalaiKholin <kholin.mikalai@gmail.com>
2023-12-25 20:37:06 +0300

    Lesson3_10

--- src/main/java/ru/aston/ActionsWithArrays.java
+++ src/main/java/ru/aston/ActionsWithArrays.java
@@ -15,4 +15,31 @@ public class ActionsWithArrays {
         }
         return arr;
     }
+
+    public static int[] createArr(int len, int initialValue){
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
+
+    public static int[] valueMultiplication(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = arr[i] < 6 ? arr[i] * 2 : arr[i];
+        }
+        return arr;
+    }
+
+    public static int[][] diagonals(int size){
+        int[][] arr = new int[size][size];
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                arr[i][j] = ((i == j) || (size - i == j + 1)) ? 1 : 0;
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+        return arr;
+    }
 }
--- src/main/java/ru/aston/Main.java
+++ src/main/java/ru/aston/Main.java
@@ -15,6 +15,8 @@ public class Main {
         System.out.println(ActionsWithDate.isLeapYear(80));
         System.out.println(Arrays.toString(ActionsWithArrays.valueChange(new int[] { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 })));
         System.out.println(Arrays.toString(ActionsWithArrays.createArr()));
-
+        System.out.println(Arrays.toString(ActionsWithArrays.valueMultiplication(new int[] { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 })));
+        ActionsWithArrays.diagonals(6);
+        System.out.println(Arrays.toString(ActionsWithArrays.createArr(5, 0)));
     }
 }
