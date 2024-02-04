f2fb874442427142b090c4c974e65ea98bdc29e9
MikalaiKholin <kholin.mikalai@gmail.com>
2024-02-04 22:55:00 +0300

    Lesson_13_1stTest

--- src/test/java/ru/aston/AppTest.java
+++ src/test/java/ru/aston/AppTest.java
@@ -1,10 +1,15 @@
 package ru.aston;
 
-import org.testng.annotations.DataProvider;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+public class AppTest extends BaseTest{
 
-public class AppTest {
-
+    @Test
+    public void testOnlinePayForm() {
+        WebElement onlinePayFormText = getDriver().findElement(By.xpath("//section[@class='pay']/div[@class='pay__wrapper']/h2"));
+        Assert.assertEquals(onlinePayFormText.getText(), "Онлайн пополнение\nбез комиссии");
+    }
 }
--- src/test/java/ru/aston/BaseTest.java
+++ src/test/java/ru/aston/BaseTest.java
@@ -1,6 +1,9 @@
 package ru.aston;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -11,11 +14,14 @@ public class BaseTest {
     public static final long PAGE_LOAD_TIMEOUT = 15;
     public static final long SCRIPT_RUN_TIMEOUT = 15;
 
-    private WebDriver driver;
+    private final WebDriver driver = new ChromeDriver();
+
+    public WebDriver getDriver() {
+        return driver;
+    }
 
     @BeforeMethod
-    protected void beforeMethod() throws InterruptedException {
-        driver.get("https://www.mts.by/");
+    protected void beforeMethod() {
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(IMPLICITLY_WAIT,
                 TimeUnit.SECONDS);
@@ -23,10 +29,14 @@ public class BaseTest {
                 TimeUnit.SECONDS);
         driver.manage().timeouts().setScriptTimeout(SCRIPT_RUN_TIMEOUT,
                 TimeUnit.SECONDS);
+        driver.get("https://www.mts.by/");
+        WebElement cookieButton = getDriver().findElement(
+                By.xpath("//button[@class ='btn btn_black cookie__ok']"));
+        cookieButton.click();
     }
 
     @AfterMethod
-    protected void afterMethod() throws InterruptedException {
-        driver.quit();
+    protected void afterMethod() {
+        //driver.quit();
     }
 }
