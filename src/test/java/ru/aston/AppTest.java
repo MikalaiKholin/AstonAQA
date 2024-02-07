d27fc6b03880a48e465d4e6633a4b9c5ba6c19e2
MikalaiKholin <kholin.mikalai@gmail.com>
2024-02-07 23:21:33 +0300

    Lesson_14_1stTest

--- src/test/java/ru/aston/AppTest.java
+++ src/test/java/ru/aston/AppTest.java
@@ -5,16 +5,12 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class AppTest extends BaseTest{
+public class AppTest extends BaseTest {
     @DataProvider(name = "onlinePayForms")
     public String[][] onlinePayForms() {
-        return new String[][] {
-                { "Услуги связи", "Номер телефона", "Сумма", "E-mail для отправки чека"},
-                { "Домашний интернет", "Номер абонента", "Сумма", "E-mail для отправки чека"},
-                { "Рассрочка", "Номер счета на 44", "Сумма", "E-mail для отправки чека"},
-                { "Задолженность", "Номер счета на 2073", "Сумма", "E-mail для отправки чека"}
-        };
+        return new String[][]{{"Услуги связи", "Номер телефона", "Сумма", "E-mail для отправки чека"}, {"Домашний интернет", "Номер абонента", "Сумма", "E-mail для отправки чека"}, {"Рассрочка", "Номер счета на 44", "Сумма", "E-mail для отправки чека"}, {"Задолженность", "Номер счета на 2073", "Сумма", "E-mail для отправки чека"}};
     }
+
     @Test(dataProvider = "onlinePayForms")
     public void testOnlinePayForms(String payment, String placeholder1, String placeholder2, String placeholder3) {
         getDriver().findElement(By.xpath("//div[@class='pay__form']//div[@class='select__wrapper']")).click();
@@ -25,6 +21,4 @@ public class AppTest extends BaseTest{
         softAssert.assertEquals(getDriver().findElement(By.xpath("//form[@class='pay-form opened']/div[3]/input")).getAttribute("placeholder"), placeholder3);
         softAssert.assertAll();
     }
-
-
 }
