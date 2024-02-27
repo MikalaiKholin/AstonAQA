8041820f93ef96d71f691a38b0d4c7b00a5e529b
MikalaiKholin <kholin.mikalai@gmail.com>
2024-02-27 16:31:34 +0300

    Lesson_17

--- src/test/java/ru/aston/AppTest.java
+++ src/test/java/ru/aston/AppTest.java
@@ -6,14 +6,12 @@ import org.testng.annotations.Test;
 import ru.aston.Base.BaseTest;
 import ru.aston.Model.*;
 
-import java.util.*;
-
 public class AppTest extends BaseTest {
   MainPage mainPage = new MainPage();
+
   private void enterExpression(String expression) {
     String[] ex = expression.split(" ");
     for (int i = 0; i < ex.length; i++) {
-      System.out.println(ex[i]);
       if (i % 2 == 0) mainPage.digitClick(ex[i]);
       else mainPage.operationClick(ex[i]);
     }
@@ -22,16 +20,18 @@ public class AppTest extends BaseTest {
   @DataProvider
   public String[][] Expressions() {
     return new String[][] {
-            {"1 + 1", "2"},
-            {"5 / 5 + 6", "7"},
-            {"2 * 3", "6"},
-            {"9 / 3", "3"},
+      {"1 + 1", "2"},
+      {"5 / 5 + 6", "7"},
+      {"2 * 3", "6"},
+      {"9 / 3", "3"},
+      {"5 - 3", "2"},
     };
   }
 
   @Test(dataProvider = "Expressions")
   public void checkBaseOperations(String expression, String expectedResult) {
     enterExpression(expression);
+    mainPage.operationClick("=");
     Assert.assertEquals(expectedResult, mainPage.getResult());
   }
 }
--- src/test/java/ru/aston/Model/MainPage.java
+++ src/test/java/ru/aston/Model/MainPage.java
@@ -1,39 +1,35 @@
 package ru.aston.Model;
 
-import ru.aston.Base.BaseTest;
-
 public class MainPage extends BasePage {
   private static final String digitsPref = "com.google.android.calculator:id/digit_";
 
   private static final String operationPref = "com.google.android.calculator:id/";
 
   public void digitClick(String digit) {
-    System.out.println(String.format("%s%s", digitsPref, digit));
-    driver.findElementById(String.format("%s%s", digitsPref, digit));
+    driver.findElementById(String.format("%s%s", digitsPref, digit)).click();
   }
 
   public void operationClick(String operation) {
-    System.out.println("operation");
     switch (operation) {
       case "+":
-        driver.findElementById(String.format("%s%s", operationPref, "op_add"));
+        driver.findElementById(String.format("%s%s", operationPref, "op_add")).click();
         break;
       case "-":
-        driver.findElementById(String.format("%s%s", operationPref, "op_sub"));
+        driver.findElementById(String.format("%s%s", operationPref, "op_sub")).click();
         break;
       case "*":
-        driver.findElementById(String.format("%s%s", operationPref, "op_mul"));
+        driver.findElementById(String.format("%s%s", operationPref, "op_mul")).click();
         break;
       case "/":
-        driver.findElementById(String.format("%s%s", operationPref, "op_div"));
+        driver.findElementById(String.format("%s%s", operationPref, "op_div")).click();
         break;
       case "=":
-        driver.findElementById(String.format("%s%s", operationPref, "eq"));
+        driver.findElementById(String.format("%s%s", operationPref, "eq")).click();
         break;
     }
   }
 
-  public String getResult(){
-      return driver.findElementById(String.format("%s%s", operationPref, "result_final")).getText();
+  public String getResult() {
+    return driver.findElementById(String.format("%s%s", operationPref, "result_final")).getText();
   }
 }
